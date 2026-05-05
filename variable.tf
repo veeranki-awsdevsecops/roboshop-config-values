232bb4f47fb5f4030b235847dd365efcb009e170
windows <veeranki2014@gmail.com>
2026-05-04 23:06:23 -0500

    roboshop project with tools

--- variable.tf
+++ variable.tf
@@ -41,6 +41,67 @@ variable "secrets" {
       }
     }
 
+    catalogue = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        MONGO       = "true"
+        MONGO_URL   = "mongodb://mongodb-dev.veerankitek.online:27017/catalogue"
+        DB_TYPE     = "mongo"
+        APP_GIT_URL = "https://github.com/roboshop-devops-project-v3/catalogue"
+        DB_HOST     = "mongodb-dev.veerankitek.online"
+        SCHEMA_FILE = "db/master-data.js"
+      }
+    }
+
+    user = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        MONGO     = "true",
+        REDIS_URL = "redis://redis-dev.veerankitek.online:6379",
+        MONGO_URL = "mongodb://mongodb-dev.veerankitek.online:27017/users"
+      }
+    }
+
+    mysql = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        ROOT_PASSWORD = "RoboShop@1"
+      }
+    }
+
+    rabbitmq = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        APP_USER     = "roboshop",
+        APP_PASSWORD = "roboshop123"
+      }
+    }
+
+    shipping = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        CART_ENDPOINT = "cart-dev.veerankitek.online:8080"
+        DB_HOST       = "mysql-dev.veerankitek.online"
+        DB_USER       = "root"
+        DB_PASS       = "RoboShop@1"
+        DB_TYPE       = "mysql"
+        APP_GIT_URL   = "https://github.com/roboshop-devops-project-v3/shipping"
+      }
+    }
+
+    payment = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        CART_HOST = "cart-dev.veerankitek.online",
+        CART_PORT = 8080,
+        USER_HOST = "user-dev.veerankitek.online",
+        USER_PORT = 8080,
+        AMQP_HOST = "rabbitmq-dev.veerankitek.online",
+        AMQP_USER = "roboshop",
+        AMQP_PASS = "roboshop123"
+      }
+    }
+
 
 
   }
