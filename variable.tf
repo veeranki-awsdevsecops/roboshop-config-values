7e6ad33b21eb1828b4a6fe6f6f011fda6a389f9c
windows <veeranki2014@gmail.com>
2026-05-04 23:01:20 -0500

    roboshop project with tools

--- variable.tf
+++ variable.tf
@@ -14,8 +14,8 @@ variable "secrets" {
     cart = {
       secret_mount = "roboshop-dev"
       kv = {
-        REDIS_HOST     = "redis-dev.rdevopsb85.online",
-        CATALOGUE_HOST = "catalogue-dev.rdevopsb85.online",
+        REDIS_HOST     = "redis-dev.veerankitek.online",
+        CATALOGUE_HOST = "catalogue-dev.veerankitek.online",
         CATALOGUE_PORT = 8080
       }
     }
@@ -23,20 +23,20 @@ variable "secrets" {
     frontend = {
       secret_mount = "roboshop-dev"
       kv = {
-        CATALOGUE_URL  = "http://catalogue-dev.rdevopsb85.online:8080/"
-        USER_URL       = "http://user-dev.rdevopsb85.online:8080/"
-        CART_URL       = "http://cart-dev.rdevopsb85.online:8080/"
-        SHIPPING_URL   = "http://shipping-dev.rdevopsb85.online:8080/"
-        PAYMENT_URL    = "http://payment-dev.rdevopsb85.online:8080/"
-        CATALOGUE_HOST = "catalogue-dev.rdevopsb85.online"
+        CATALOGUE_URL  = "http://catalogue-dev.veerankitek.online:8080/"
+        USER_URL       = "http://user-dev.veerankitek.online:8080/"
+        CART_URL       = "http://cart-dev.veerankitek.online:8080/"
+        SHIPPING_URL   = "http://shipping-dev.veerankitek.online:8080/"
+        PAYMENT_URL    = "http://payment-dev.veerankitek.online:8080/"
+        CATALOGUE_HOST = "catalogue-dev.veerankitek.online"
         CATALOGUE_PORT = "8080"
-        USER_HOST      = "user-dev.rdevopsb85.online"
+        USER_HOST      = "user-dev.veerankitek.online"
         USER_PORT      = "8080"
-        CART_HOST      = "cart-dev.rdevopsb85.online"
+        CART_HOST      = "cart-dev.veerankitek.online"
         CART_PORT      = "8080"
-        SHIPPING_HOST  = "shipping-dev.rdevopsb85.online"
+        SHIPPING_HOST  = "shipping-dev.veerankitek.online"
         SHIPPING_PORT  = "8080"
-        PAYMENT_HOST   = "payment-dev.rdevopsb85.online"
+        PAYMENT_HOST   = "payment-dev.veerankitek.online"
         PAYMENT_PORT   = "8080"
       }
     }
